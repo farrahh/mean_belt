b11608ca3f34e365ab0af77be7e78fa3d820239f
Farrah Hashmi <farrahhhashmi@gmail.com>
2017-04-05 14:00:32 -0700

    with out heroku

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -2,8 +2,8 @@ var mongoose = require('mongoose');
 
 var fs = require('fs');
 
-// mongoose.connect('mongodb://localhost/BlackBelt');
-mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://mean_belt:mean_belt@ds153400.mlab.com:53400/heroku_ck62nsbz');
+mongoose.connect('mongodb://localhost/BlackBelt');
+// mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://mean_belt:mean_belt@ds153400.mlab.com:53400/heroku_ck62nsbz');
 
 var models_path = __dirname + '/../models'
 
